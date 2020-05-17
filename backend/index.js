a583df28215450656bad9c783f846821825ca61c
JesusRRr <rolonjesus1996@gmail.com>
2020-05-16 19:13:10 -0500

    chore : configure multer: its a middleware to handle images uploaded to the server

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
-const multer = require('multer')
+const multer = require('multer');
+const path = require('path');
 
 const app = express();
 app.set('port',3000);
 
 
 //Middlewares
-app.use(morgan('dev'));
+app.use(morgan('dev'));//messages from server
+const storage =multer.diskStorage({
+    destination:path.join(__dirname,'public/uploads'),
+    filename(req, file, cb){
+        cb(null,new Date().getTime()+path.extname(file.originalname));
+    }
+})
+app.use(multer({storage}).single('image'));//used for manage images
 
 //Start server
 app.listen(app.get('port'), ()=>{

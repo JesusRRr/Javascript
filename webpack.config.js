57b3eb1425bfa5daf7a3b63beb5858028c3ce32f
JesusRRr <rolonjesus1996@gmail.com>
2020-05-21 12:07:31 -0500

    feature: design form

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,8 @@ module.exports ={
         path:path.join(__dirname,'backend/public'),
         filename: 'js/bundle.js'
     },
+    mode: 'development',
+    
     module:{
         rules:[
             {
